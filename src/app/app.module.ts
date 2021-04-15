488cdf68d69923a63b99848a5ec1607e03f6911e
Vardhan-goud <chintu.vardhan23@gmail.com>
2021-04-15 13:54:51 +0530

    structured design made

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,40 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { AllcontactsdataService } from './allcontactsdata.service';
-
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { FormpopupComponent } from './formpopup/formpopup.component';
-import { HomeComponent } from './home/home.component';
-import { AddComponent } from './add/add.component';
-import {FormsModule} from '@angular/forms';
-import {ReactiveFormsModule} from '@angular/forms';
-import { ContactsviewComponent } from './contactsview/contactsview.component';
-import { DisplaayActiveContactComponent } from './displaay-active-contact/displaay-active-contact.component'
+import {AppComponent,NavigationComponent} from './Components';
+import { CareerModule } from './careers/career.module';
 
 
 @NgModule({
-
   declarations: [
     AppComponent,
-    FormpopupComponent,
-    HomeComponent,
-    AddComponent,
-    ContactsviewComponent,
-    DisplaayActiveContactComponent
+    NavigationComponent
   ],
-
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule,
-    ReactiveFormsModule
+    CareerModule
   ],
-
-  providers: [AllcontactsdataService],
-  
+  providers: [],
   bootstrap: [AppComponent]
-
 })
-
 export class AppModule { }
